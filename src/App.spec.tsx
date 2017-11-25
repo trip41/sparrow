48de6aa55018f0e1abd8b83adc1285f979fbb249
Ryan Rowe <rrowe@palantir.com>
2017-11-25 17:19:35 -0500

    bootstrap proejct, add typescript

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "react-native";
+import App from "./App";
+
+// Note: test renderer must be required after react-native.
+import renderer from "react-test-renderer";
+
+it("renders correctly", () => {
+    const tree = renderer.create(
+        <App />,
+    );
+    expect(tree).toBeDefined();
+});
